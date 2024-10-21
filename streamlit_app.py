df154bd8da8d51487615a7ec397533403d29504f
Eric Chiu <wf870125@gmail.com>
2024-10-21 01:12:43 -0700

    title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,8 @@ SHEET_ID = '1P4qLMUxjbXFY2-FtoIQNhSdcogLgoE0aM1yjvaQbt3Y'
 SHEET_NAME = 'Leaderboard'
 url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
 df = pd.read_csv(url)
+
+
+st.title("TNA Cup Test")
 st.dataframe(df)
 # st.markdown(df.head())
