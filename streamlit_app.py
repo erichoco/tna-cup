3c3ade1e16d4e18d38f41f9b172d66fd63265915
Eric Chiu <wf870125@gmail.com>
2024-10-21 01:29:17 -0700

    simple layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,22 @@ url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sh
 df = pd.read_csv(url)
 
 
-st.title("TNA Cup Test")
-st.dataframe(df)
-# st.markdown(df.head())
+# def main():
+st.title("TNA Cup")
+with st.container():
+    st.video("https://www.youtube.com/live/kp_W_j-YCzg?si=hspCSsW3L-9tg_ZN")
+    
+col1, col2= st.columns(2)
+with col1:
+    st.header("最新對戰")
+
+with col2:
+    st.header("積分榜")
+    st.dataframe(df.head().iloc[:, 1:12])
+
+
+# def roster():
+#     st.title("玩家介紹")    
+    
+# pg = st.navigation([st.Page(main), st.Page(roster)])
+# pg.run()
